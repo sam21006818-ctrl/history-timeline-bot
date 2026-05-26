4788b902c402e42ca8ee32eddff3492241d1ea24
sam21006818-ctrl <sam21006818@gmail.com>
2026-05-26 10:01:46 +0800

    Refactor data loading and update tooltip information

--- web_app.py
+++ web_app.py
@@ -2,23 +2,17 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 
-# 網頁基本設定
 st.set_page_config(page_title="全台謝氏宗親會互動地圖", page_icon="⛩️", layout="wide")
 st.title("⛩️ 全台謝氏宗親會互動地圖")
 st.markdown("請將滑鼠游標移至地圖上的 **橘色圓點**，即可自動查看該地區的宗親會詳細資訊！")
 st.markdown("---")
 
-# 嘗試讀取抓下來的資料，如果沒有就用預設資料
 try:
     from clan_data import get_clan_data
     df = get_clan_data()
 except:
-    data = [
-        {"名稱": "世界謝氏宗親總會", "負責人": "謝宗翰", "地址": "台北市中山區吉林路286號", "緯度": 25.0645, "經度": 121.5295, "簡介": "全國性宗親總會"}
-    ]
-    df = pd.DataFrame(data)
+    df = pd.DataFrame([{"名稱": "系統更新中", "負責人": "稍後", "電話": "稍後", "傳真": "稍後", "地址": "讀取中...", "緯度": 23.6, "經度": 120.9, "簡介": "請稍候"}])
 
-# 🚀 建立滑鼠可互動的地圖圖層
 layer = pdk.Layer(
     "ScatterplotLayer",
     df,
@@ -34,32 +28,23 @@ layer = pdk.Layer(
     get_line_color=[255, 255, 255],
 )
 
-# 設定地圖一開始看的位子 (鎖定台灣中心點)
-view_state = pdk.ViewState(
-    latitude=23.7,
-    longitude=121.0,
-    zoom=6.5,
-    pitch=0
-)
+view_state = pdk.ViewState(latitude=23.7, longitude=121.0, zoom=6.5, pitch=0)
 
-# 🚀 畫出地圖，並設定滑鼠移過去彈出的資訊卡 (Tooltip)
 st.pydeck_chart(pdk.Deck(
     layers=[layer],
     initial_view_state=view_state,
     tooltip={
         "html": """
-            <div style="font-family: sans-serif; padding: 5px;">
+            <div style="font-family: sans-serif; padding: 5px; line-height: 1.5;">
                 <b style="font-size: 16px; color: #FF8C00;">⛩️ {名稱}</b><br/>
                 <b>👨‍💼 負責人：</b> {負責人}<br/>
-                <b>🏠 地址：</b> {地址}<br/>
+                <b>📞 電 話：</b> {電話}<br/>
+                <b>📠 傳 真：</b> {傳真}<br/>
+                <b>🏠 地 址：</b> {地址}<br/>
                 <hr style="margin: 5px 0; border: 0.5px solid #ccc;"/>
                 <span style="font-size: 12px; color: #ddd;">{簡介}</span>
             </div>
         """,
-        "style": {
-            "backgroundColor": "#222222", 
-            "color": "white",
-            "borderRadius": "8px"
-        }
+        "style": {"backgroundColor": "#222222", "color": "white", "borderRadius": "8px"}
     }
 ))
